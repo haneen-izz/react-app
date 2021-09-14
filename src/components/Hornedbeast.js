99be53404b7feec86686ef1a4f575bad67935c16
haneen <haneenjaradeh1@gmail.com>
2021-09-14 15:31:24 +0300

    changes

--- src/components/Hornedbeast.js
+++ src/components/Hornedbeast.js
@@ -1,42 +1,46 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
 
 class Hornedbeast extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-    numOfclick:0
-    }
-  }
-     
-    increasenumOfclick = () => {
-    this.setState({
-    numOfclick:this.state.numOfclick + 1,
-    })
+    this.state = {
+      numOfclick: 0,
+    };
   }
 
-    render() {
+  increasenumOfclick = () => {
+    this.setState({
+      numOfclick: this.state.numOfclick + 1,
+    });
+  };
+
+  render() {
     return (
-    <div>
-    <Col>
-    <Card className= "card"  > 
-    <Card.Img variant="top" src= {this.props.img} />  
-    <Card.Body >
-    <Card.Title>{this.props.title}</Card.Title>
-    <Card.Text> number of clicks {this.state.numOfclick} ​​​​​​​​</Card.Text>
-    <Button onClick = {this.increasenumOfclick} variant="primary">
-     Vote​​​​​​​​
-    </Button>
-    </Card.Body>
-    </Card>
-    </Col>
-    </div>
-        );
-    }
+      <div>
+        <Col>
+          <Card className="card">
+            <Card.Img className="cardi" variant="top" src={this.props.img} />
+            <Card.Body>
+              <Card.Title>{this.props.title}</Card.Title>
+              <Card.Text>
+                {" "}
+                number of clicks {this.state.numOfclick} ​​​​​​​​
+              </Card.Text>
+              <Button
+                onClick={this.increasenumOfclick}
+                variant="primary"
+              >
+                Vote ​​​​​​​​
+              </Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </div>
+    );
   }
-    export default Hornedbeast;
-    
+}
+export default Hornedbeast;
