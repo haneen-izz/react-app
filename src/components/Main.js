b06d9c2d264b8b9b7296acca74fa018e1edf5786
haneen <haneenjaradeh1@gmail.com>
2021-09-14 15:02:05 +0300

    done

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,32 @@
 
-import React from "react";
-import Hornedbeast from "./Hornedbeast";
-import data from "./data.json";
-import Row from "react-bootstrap/Row";
+import React from 'react';
+import Hornedbeast from './Hornedbeast';
+import data from './data.json';
+import Row from 'react-bootstrap/Row';
  
-class Main extends React.Component {​​​​​​​​
-render() {​​​​​​​​
-return (
-<div>
-<Row xs= {​​​​​​​​1} ​​​​​​​​ md = {​​​​​​​​3}​​​​​​ className ="g-4"> {​​​​​​​​data.map((val, index) => {​​​​​​​​
-return ( 
-<Hornedbeast key={​​​​​​​​index}​​​​​​​​ title={​​​​​​​​val.title}​​​​​​​​ img={​​​​​​​​val.image_url}​​​​​​ description={​​​​​​​​val.description}​​​​​​​​ keyWord={​​​​​​​​val.keyWord}​​​​​​​​ horns ={​​​​​​​​val.horns}​​​​​​​
-/>
+class Main extends React.Component{
+render(){
+return(
+<div> 
+​​​​​​​​<Row xs={6} md={2} className="g-4" >
+{
+  data.map((val , index) => {
+  return (
+<Hornedbeast key={index} title= {val.title} 
+img ={val.image_url}
+description= {val.title}
+keyWord={val.leyWord} horns = {val.horns}/>
+
+    );
+
+  }
+  )
+}
+</Row> </div>
  );
-  }​​)}​​​​​​​​
-</Row> 
-</div> );
-  }​​​​​​​​
-}​​​​​​​​
+
+}
+}
+
 export default Main;
 
